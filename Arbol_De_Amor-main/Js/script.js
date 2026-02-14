3126921b789e8b8ede8852275701652965f30c5c
Howking77 <stremintel@gmail.com>
2026-02-14 11:46:32 -0500

    Update anniversary message format in script.js

--- Arbol_De_Amor-main/Js/script.js
+++ Arbol_De_Amor-main/Js/script.js
@@ -160,8 +160,8 @@ function showCountdown() {
     let eventSeconds = Math.max(0, Math.floor((eventDiff / 1000) % 60));
 
     container.innerHTML =
-      `Llevamos juntos: <b>${days}</b> días<br>` +
-      `Nuestro aniversario: <b>${eventDays}d ${eventHours}h ${eventMinutes}m ${eventSeconds}s</b>`;
+      `Desde hace: <b>${days}</b> días<br>` +
+      `Un año mas en: <b>${eventDays}d ${eventHours}h ${eventMinutes}m ${eventSeconds}s</b>`;
     container.classList.add('visible');
   }
   update();
